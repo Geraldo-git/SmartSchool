3b90aec3266ccbfccc2fdfbd3e4da39d95e26f07
Geraldo-git <laboratorio.inicial@gmail.com>
2023-03-02 16:55:34 -0300

    Ajustes e criacao dos priomeiros controllers

---
+++ SmartSchool/SmartSchool.API/Controllers/AlunoController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SmartSchool.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlunoController : ControllerBase
+    {
+        public AlunoController()
+        {
+
+        }
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Alunos: Raul, Erivania, Nilia, Evilasio");
+        }
+    }
+}
